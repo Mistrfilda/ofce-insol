79f5e24581a21aa64748827d583cd7d220275ab4
Filip Kuchár <filda.kuchar@seznam.cz>
2018-06-11 00:06:25 +0200

    tests.neon fix

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -21,12 +21,8 @@ $robotLoader->acceptFiles = '*.php, *.phpt';
 $robotLoader->register();
 
 $configurator->addConfig(__DIR__ . '/../app/config/config.neon');
-$configurator->addConfig(__DIR__ . '/tests.neon');
+$configurator->addConfig(__DIR__ . '/../app/config/config.local.neon');
 
-//just for localhost
-if (is_dir(__DIR__ . '/../app/config/config.local.neon')) {
-	$configurator->addConfig(__DIR__ . '/../app/config/config.local.neon');
-}
 
 $container = $configurator->createContainer();
 
