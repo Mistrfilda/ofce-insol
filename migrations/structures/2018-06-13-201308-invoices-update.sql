3042ddef27daa591a09888344f3d9e5a638287b7
Filip Kuchár <filda.kuchar@seznam.cz>
2018-06-13 20:24:19 +0200

    migrations fix

--- migrations/structures/2018-06-13-201308-invoices-update.sql
+++ migrations/structures/2018-06-13-201308-invoices-update.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `invoices` ADD COLUMN `invoices_persons_system_id` INT UNSIGNED NULL DEFAULT;
+ALTER TABLE `invoices` ADD COLUMN `invoices_persons_system_id` INT UNSIGNED NULL DEFAULT NULL;
